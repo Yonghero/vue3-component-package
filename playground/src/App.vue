149bfed8018560ad3103ac7c629923ceb3503ae6
younghero <yxzhang@hitotek.com>
2023-04-06 17:20:00 +0800

    feat: resolve vue3 component package

---
+++ playground/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RollupComponent from '../../dist/index.vue'
+</script>
+
+<template>
+  <div>
+    <RollupComponent />
+  </div>
+</template>
---
+++ src/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="'vue-component-rollup'">
+    {{ msg }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref('hello')
+</script>
